2385d3c461e429f64820014975684b64a59896ee
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2023-07-07 16:40:15 +0200

    Styles, button, footer

--- index.php
+++ index.php
@@ -7,18 +7,15 @@
         <h1>Bienvenue sur le site Useless Knowledge !</h1>
         <h2>Vous trouverez ici des informations inutiles sur des sujets inutiles.</h2>
 
-        <form action="index.php" method="post">
-            <input type="submit" name="submit" value="Afficher une information inutile">
-        </form>
         <?php 
+            //on compte le nombre de ligne du fichier
+            $file = fopen("infos.txt", "r");
+            $lineCount = 0;
+            while(!feof($file)){
+                $line = fgets($file);
+                $lineCount++;
+            }
             if(isset($_POST['submit'])){
-                $file = fopen("infos.txt", "r");
-                $lineCount = 0;
-                while(!feof($file)){
-                    $line = fgets($file);
-                    $lineCount++;
-                }
-                echo "Il y a ".$lineCount." lignes dans le fichier.<br>";
                 //on génère un nombre aléatoire entre 1 et le nombre de ligne du fichier
                 $random = rand(1, $lineCount);
                 //on lit et retourne la ligne correspondant au nombre aléatoire
@@ -28,10 +25,16 @@
                     $line = fgets($file);
                     $lineCount2++;
                     if($lineCount2 == $random){
-                        echo "<div class='info'>".$line."</div>";
+                        echo "<div class='container'><div class='info'>".$line."</div></div>";
                     }
                 }
             }
+            echo "<div class='fin'><div class='container'>Le fichier comporte actuellement ".$lineCount." informations inutiles.</div></div>";
         ?>
+        <form action="index.php" method="post">
+            <div class="container">
+                <button type="submit" name="submit" class="button"><span class="button-content">Afficher une information inutile</span></button>
+            </div>
+        </form>
     </body>
 </html>
